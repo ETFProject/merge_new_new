5b51a3859b0e348a6ab3030fe9ae64c23ae51cd1
Lucky7 <lucky7@example.com>
2025-06-22 23:58:54 -0700

    fix(charts): enlarge orbital view to fill container

--- src/components/3d/ThreeOrbitalView.tsx
+++ src/components/3d/ThreeOrbitalView.tsx
@@ -113,7 +113,7 @@ function Scene({ data }: ThreeOrbitalViewProps) {
 export function ThreeOrbitalView({ data }: ThreeOrbitalViewProps) {
   return (
     <div className="w-full h-full rounded-lg overflow-hidden">
-      <Canvas camera={{ position: [0, 4, 10], fov: 50 }}>
+      <Canvas camera={{ position: [0, 2, 5], fov: 50 }}>
         <Scene data={data} />
       </Canvas>
     </div>
